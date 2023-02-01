aec090426689ae255386d84fb2ac87e38d97dcc0
ramu <nirudiramu118@gmail.com>
2023-02-01 09:48:29 +0000

    first project using reactjs

---
+++ index.js
@@ -0,0 +1,18 @@
+const element = (
+  <div className="bg-container">
+    <h1 className="heading"> Super Over League </h1>
+    <div>
+      <img
+        src=" https://assets.ccbp.in/frontend/react-js/rcb-img.png "
+        className="pic"
+      />
+      <img
+        src=" https://assets.ccbp.in/frontend/react-js/csk-img.png"
+        className="pic"
+      />
+      >
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
